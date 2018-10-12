57bdbf46576dd4b4e4de4315c41499127a6b0043
Ruben Tzab <rtzab@kwantec.com>
2018-10-12 17:22:35 -0500

    Added more partials, styles and jasmine for testing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Backbone	from 'backbone';
 import userDashboard from './views/userDashboard';
 import './scss/main.scss';
+// import '../node_modules/foundation-sites/dist/css/foundation.min.css';
+// import foundation from '../node_modules/foundation-sites/dist/js/foundation.min.js';
 
 const userDashboardView = new userDashboard({
 	el: '#user-details',
